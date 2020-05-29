b3938a2f7e80cc59711eaed55ef2f66de8ed7d3d
saphya-el-ouahabi <56952563+saphya-el-ouahabi@users.noreply.github.com>
2020-05-29 10:57:23 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+/*CLASSE MAIN*/	
+
+
+public class Main {
+
+    public static void main(String[] args) {
+    	//ouvre l'interface menu 
+        InterfaceMenu menu = new InterfaceMenu();
+    }
+
+}
